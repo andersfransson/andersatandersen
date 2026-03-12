eedc7277ceeebc871ff84e9ac847f83d05f04f07
Anders Fransson <anders.fransson@arway.se>
2026-03-12 19:33:58 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
-// @ts-check
-
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://andersatandersen.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://andersatandersen.com",
+  output: "static"
 });
