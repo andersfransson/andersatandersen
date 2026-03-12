67ed1b6e721728bfe6d5647228cbcebc5ff5655f
Anders Fransson <anders.fransson@arway.se>
2026-03-12 13:47:41 +0000

    Add GitHub Pages deployment for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://andersatandersen.com',
 	integrations: [mdx(), sitemap()],
 });
