6684872091a6186ba5b4ed359e6ff1f99fd4fd2f
Anders Fransson <anders.fransson@arway.se>
2026-03-13 05:18:12 +0100

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,1 +1,15 @@
+import { defineCollection, z } from "astro:content";
 
+const insights = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    topic: z.string(),
+    summary: z.string(),
+    date: z.string()
+  })
+});
+
+export const collections = {
+  insights
+};
